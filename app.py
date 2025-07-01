7daadf9382d999f0181060559df3251e95057bed
DanCalebMeloGutirrez2 <dan.melo@unmsm.edu.pe>
2025-07-01 18:50:55 -0500

    Nature Sale

--- app.py
+++ app.py
@@ -107,8 +107,5 @@ def nosotros():
 def contactanos():
     return render_template('empresa/contactanos.html')
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # 5000 para desarrollo local
-    app.run(host='0.0.0.0', port=port, debug=True)
-
+    app.run(debug=True)
